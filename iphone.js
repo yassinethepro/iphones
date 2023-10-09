d5577a278d6427a9cf138e752051ca9f293d5742
Yassine <yassinbakoir@gmail.com>
2023-10-09 16:20:24 -0700

    first commit

---
+++ iphone.js
@@ -0,0 +1,14 @@
+let mainImage = document.getElementById('main-img');
+let container = document.querySelector('.container')
+let img = document.getElementById('img0')
+
+
+
+
+ function phones(phoneSrc){
+    mainImage.src = phoneSrc.src;
+}
+
+function colors (color){
+    body.style.background = color;
+}
